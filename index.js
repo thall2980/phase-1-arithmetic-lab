e95c417145a65bd20c5b82d7599ff763d1ecdcaa
Tyler Hall <thall2980@gmail.com>
2022-08-29 20:39:38 -0400

    commit tree d6f33d6223133dff5bf23160cef9280bdf6933b9

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+let multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 19
+const num4 = 5
+
+let mod = num3 % num4
+
+let max = Math.max(1, 2 , 4, 20)
